1dda9ca8c857cd344a236f7a30efa5c79f26f9b7
nexus85 <mx38@mail.ru>
2016-08-26 22:54:25 +0300

    создание класса

--- src/Animal.java
+++ src/Animal.java
@@ -1,7 +1,14 @@
-package PACKAGE_NAME;
-
 /**
  * Created by nexus on 26.08.16.
  */
 public class Animal {
+
+    int age;
+    String Name;
+
+    public void say() {
+        System.out.println("ttt");
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Good!!!!");
+        System.out.println("Good!!!!");
 
-        System.out.println("Good!!!!");
-        System.out.println("Good!!!!");
+        Animal animal = new Animal();
+        animal.age = 55;
+        animal.Name = "bobr";
+        animal.say();
+
     }
 }
