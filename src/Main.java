2c1bd87cf178469c0db732244f44b3f3088c7734
nexus85 <mx38@mail.ru>
2016-08-26 22:44:04 +0300

    Разработки дополнительный комит

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-package PACKAGE_NAME;
-
 /**
  * Created by nexus on 26.08.16.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        System.out.println("Good!!!!");
+        System.out.println("Good!!!!");
+    }
 }
